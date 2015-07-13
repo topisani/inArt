cba9b95cfb7cb5c6b91a87309bf56fe196088b70
Tobias Pisani <tobiaspisani@gmail.com>
2015-07-13 10:35:01 +0200

    Added functions for handling login attempts

--- includes/functions.php
+++ includes/functions.php
@@ -224,6 +224,60 @@ function get_user_info($login) {
 		return compact ( 'user_id', 'email', 'username', 'password', 'salt' );
 	return false;
 }
+/**
+ * Add a failed login atempt for given user
+ * @param $user_id
+ */
+function add_brute($user_id) {
+	global $mysqli;
+	$now = time ();
+	$mysqli->query ( "INSERT INTO login_attempts(user_id, time)
+			VALUES ('$user_id', '$now')" );
+}
+/**
+ * Remove all failed login atempts
+ * 
+ * @param unknown $user_id
+ * @return int amount of attempts removed    	
+ */
+function reset_brute($user_id) {
+	global $mysqli;
+	$mysqli->query ( "DELETE FROM login_attempts WHERE user_id = '$user_id'" );
+	return $mysqli->affected_rows;
+}
+/**
+ * Check wether a user is locked due to too many wrong attempts
+ *
+ * @param integer $user_id
+ *        	the id of the user to check for
+ * @return boolean
+ */
+function checkbrute($user_id) {
+	global $mysqli;
+	// Get timestamp of current time
+	$now = time ();
+
+	// All login attempts are counted from the past 2 hours.
+	$valid_attempts = $now - (2 * 60 * 60);
+
+	if ($stmt = $mysqli->prepare ( "SELECT time
+			FROM login_attempts
+			WHERE user_id = ?
+			AND time > '$valid_attempts'" )) {
+			$stmt->bind_param ( 'i', $user_id );
+
+			// Execute the prepared query.
+			$stmt->execute ();
+			$stmt->store_result ();
+
+			// If there have been more than [LOGIN_ATEMPTS] atempts
+			if (($stmt->num_rows > LOGIN_ATEMPTS) && ((LOGIN_ATEMPTS) != 0)) {
+				return true;
+			} else {
+				return false;
+			}
+	}
+}
 // ####################################################
 // LOGIN & REGISTER
 // ####################################################
@@ -261,11 +315,9 @@ function login($login, $password) {
 	
 	if (checkbrute ( $user_id, $mysqli ) == true) {
 		// Account is locked
-		// Send an email to user saying their account is locked
+		// TODO some way to unlock
 		return '3';
 	} else {
-		// Check if the password in the database matches
-		// the password the user submitted.
 		if ($db_password == $password) {
 			// Password is correct!
 			// Get the user-agent string of the user.
@@ -281,45 +333,8 @@ function login($login, $password) {
 			return true;
 		} else {
 			// Password is not correct
-			// We record this attempt in the database
-			$now = time ();
-			$mysqli->query ( "INSERT INTO login_attempts(user_id, time)
-        					VALUES ('$user_id', '$now')" );
+			add_brute($user_id);
 			return '1';
-		}
-	}
-}
-
-/**
- * Check wether a user is locked due to too many wrong attempts
- *
- * @param integer $user_id
- *        	the id of the user to check for
- * @return boolean
- */
-function checkbrute($user_id) {
-	global $mysqli;
-	// Get timestamp of current time
-	$now = time ();
-	
-	// All login attempts are counted from the past 2 hours.
-	$valid_attempts = $now - (2 * 60 * 60);
-	
-	if ($stmt = $mysqli->prepare ( "SELECT time
-			FROM login_attempts
-			WHERE user_id = ?
-			AND time > '$valid_attempts'" )) {
-		$stmt->bind_param ( 'i', $user_id );
-		
-		// Execute the prepared query.
-		$stmt->execute ();
-		$stmt->store_result ();
-		
-		// If there have been more than [LOGIN_ATEMPTS] atempts
-		if (($stmt->num_rows > LOGIN_ATEMPTS) && ((LOGIN_ATEMPTS) != 0)) {
-			return true;
-		} else {
-			return false;
 		}
 	}
 }
